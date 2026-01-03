828ad0b08797709164a3f70a18f5e5f306b3849b
harpreetsaini1111-dot <harpreetsaini1111@gmail.com>
2026-01-03 08:04:42 +0530

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,6 +1,12 @@
 import os
 from google.cloud import storage
 
+BUCKET_NAME = os.environ.get("GCS_BUCKET")
+
 def get_bucket():
-    client = storage.Client()
-    return client.bucket(os.environ["GCS_BUCKET"])
+    if not BUCKET_NAME:
+        raise RuntimeError("GCS_BUCKET environment variable not set")
+
+    client = storage.Client()  # uses GOOGLE_APPLICATION_CREDENTIALS
+    bucket = client.bucket(BUCKET_NAME)
+    return bucket
